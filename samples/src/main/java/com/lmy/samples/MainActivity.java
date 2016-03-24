af32254a9d2a24e3ffc565d81ac2d969f62fd0d4
lrlmy <425078310@qq.com>
2016-03-24 18:35:27 +0800

    完善sample

--- samples/src/main/java/com/lmy/samples/MainActivity.java
+++ samples/src/main/java/com/lmy/samples/MainActivity.java
@@ -2,12 +2,16 @@ package com.lmy.samples;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.lmy.lymedia.utils.Util;
 import com.lmy.lymedia.widget.VideoSurfaceView;
 
 public class MainActivity extends AppCompatActivity {
     private VideoSurfaceView mSurfaceView;
+    private Button oneBtn;
+    private Button twoBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +19,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mSurfaceView = (VideoSurfaceView) findViewById(R.id.surface);
         mSurfaceView.initPlayer(Util.getSdcardPath() + "/test.mp4");
+        oneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSurfaceView.releasePlaer();
+                mSurfaceView.initPlayer(Util.getSdcardPath() + "/test.mp4");
+            }
+        });
+        oneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSurfaceView.releasePlaer();
+                mSurfaceView.initPlayer(Util.getSdcardPath() + "/test1.mp4");
+            }
+        });
     }
 }
