33d2dccab90f87c855d770e76c322b5128870d6c
lrlmy <425078310@qq.com>
2016-03-28 14:14:43 +0800

    测试

--- samples/src/main/java/com/lmy/samples/MainActivity.java
+++ samples/src/main/java/com/lmy/samples/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         mSurfaceView = (VideoSurfaceView) findViewById(R.id.surface);
         oneBtn = (Button) findViewById(R.id.button1);
         twoBtn = (Button) findViewById(R.id.button2);
-        mSurfaceView.initPlayer(Util.getSdcardPath() + "/test.flv");
+        mSurfaceView.initPlayer(Util.getSdcardPath() + "/test.mp4");
         oneBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
