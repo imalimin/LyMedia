c2c490c44ba34613996e4418df49b5dbaa462538
mingyi <mingyil@l99.com>
2016-04-08 17:02:36 +0800

    滤镜！！！

--- library/src/main/java/com/lmy/lymedia/media/render/FilterRander.java
+++ library/src/main/java/com/lmy/lymedia/media/render/FilterRander.java
@@ -2,9 +2,13 @@ package com.lmy.lymedia.media.render;
 
 import android.util.Log;
 
+import com.lmy.lymedia.utils.Util;
+
 import org.bytedeco.javacv.FFmpegFrameFilter;
 import org.bytedeco.javacv.Frame;
 import org.bytedeco.javacv.FrameFilter;
+
+import static org.bytedeco.javacpp.avutil.AV_PIX_FMT_RGBA;
 
 /**
  * Created by lifeix on 2016/4/8.
@@ -13,7 +17,8 @@ public class FilterRander implements Render {
     private FFmpegFrameFilter mFilter;
 
     public FilterRander(int width, int height) {
-        mFilter = new FFmpegFrameFilter("transpose=clock", width, height);
+        mFilter = new FFmpegFrameFilter("curves=psfile='" + Util.getSdcardPath() + "/FA_Curves2.acv'", width, height);
+        mFilter.setPixelFormat(AV_PIX_FMT_RGBA);
         try {
             mFilter.start();
         } catch (FrameFilter.Exception e) {
@@ -25,7 +30,7 @@ public class FilterRander implements Render {
     public Frame render(Frame frame) {
         try {
             mFilter.push(frame);
-            while ((frame = mFilter.pull()) != null) {
+            while ((frame = mFilter.pull()) == null) {
                 Log.v("FilterRander", "wait render");
             }
         } catch (FrameFilter.Exception e) {
