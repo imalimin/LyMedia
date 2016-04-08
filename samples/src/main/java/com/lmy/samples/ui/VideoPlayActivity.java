25cc36e66173bd98ebcf1a3646ea2cd2bf68872b
mingyi <mingyil@l99.com>
2016-04-08 14:24:43 +0800

    独立视频播放activity

---
+++ samples/src/main/java/com/lmy/samples/ui/VideoPlayActivity.java
@@ -0,0 +1,46 @@
+package com.lmy.samples.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.lmy.lymedia.utils.Util;
+import com.lmy.lymedia.widget.VideoSurfaceView;
+import com.lmy.samples.R;
+
+public class VideoPlayActivity extends AppCompatActivity {
+    private VideoSurfaceView mSurfaceView;
+    private Button oneBtn;
+    private Button twoBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_play);
+        mSurfaceView = (VideoSurfaceView) findViewById(R.id.surface);
+        oneBtn = (Button) findViewById(R.id.button1);
+        twoBtn = (Button) findViewById(R.id.button2);
+        mSurfaceView.initPlayer(Util.getSdcardPath() + "/test.f4v");
+        oneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSurfaceView.releasePlaer();
+                mSurfaceView.initPlayer(Util.getSdcardPath() + "/test.mp4");
+            }
+        });
+        twoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSurfaceView.releasePlaer();
+                mSurfaceView.initPlayer(Util.getSdcardPath() + "/test.f4v");
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mSurfaceView.releasePlaer();
+    }
+}
